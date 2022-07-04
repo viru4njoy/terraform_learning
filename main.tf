4a683b36c3fab4e681e01f4d28ca8a10a72e4aea
viru4njoy <veerendrareddy24@gmail.com>
2022-07-04 02:57:13 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws"{
+        region= "us-east-2"
+}
+resource "aws_instance" "myfirstserver" {
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+
+    tags={
+        Name= "myfirstserver"
+    }
+}
